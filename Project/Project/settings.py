921e67360699da18495d73c146e7621f193f23e3
harshalpatil2809 <harshalpatil28095@gmail.com>
2025-12-10 22:53:39 +0530

    add tailwind

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join[BASE_DIR, 'static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Tailwind Files 
+TAILWIND_APP_NAME = 'theme'
