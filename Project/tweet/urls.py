d584710c85e66894e4d70f03e3137f05571f9769
harshalpatil2809 <harshalpatil28095@gmail.com>
2025-12-20 23:14:24 +0530

    20-12-2025

--- Project/tweet/urls.py
+++ Project/tweet/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.tweet, name='tweet'),
     path('showtweet/', views.Showtweet, name='showtweet'),
     path('create/', views.CreateTweet, name='Create_T'),
-    path('delete/', views.DeleteTweet, name='Delete_T')
+    path('edit/<int:tweet_id>/', views.EditTweet, name='Edit_T'),
+    path('delete/<int:tweet_id>/', views.DeleteTweet, name='Delete_T')
 ] 
